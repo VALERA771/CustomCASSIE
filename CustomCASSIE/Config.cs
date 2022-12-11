c26be7049651e3c2fccbe5d199e945aaa482946f
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2022-12-11 13:32:36 +0300

    Added files

---
+++ CustomCASSIE/Config.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace CustomCASSIE
+{
+    public sealed class Config : IConfig
+    {
+        [Description("Is plugin enabled?")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Text for cassie")]
+        public string Message { get; set; } = "";
+
+        [Description("Should cassie have subtitels?")]
+        public bool IsSubtiteled { get; set; } = true;
+
+        [Description("Time between LCZ decontamination start and anouchement")]
+        public int WaitTime { get; set; } = 10;
+    }
+}
---
+++ CustomCASSIE/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace CustomCASSIE
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Custom CASSIE";
+        public override string Prefix => "CustomCASSIE";
+        public override string Author => "VALERA771#1471";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(5, 3, 0);
+
+        private MapH map;
+
+        public override void OnEnabled()
+        {
+            map = new MapH();
+            Exiled.Events.Handlers.Map.Decontaminating += map.OnDecont;
+            Plugin.singleton = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            map = null;
+            Exiled.Events.Handlers.Map.Decontaminating -= map.OnDecont;
+
+            Plugin.singleton = null;
+            base.OnDisabled();
+        }
+
+
+        public static Plugin Instance => singleton;
+        private static Plugin singleton;
+    }
+}
