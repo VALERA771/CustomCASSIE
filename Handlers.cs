d3e3584088ece4bef28166a8cf86cba0962af5fb
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2022-12-13 20:57:51 +0300

    I DID THIS!!!

---
+++ Handlers.cs
@@ -0,0 +1,54 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Linq;
+
+namespace CustomCASSIE
+{
+    public class Handlers
+    {
+        public void OnDecont(DecontaminatingEventArgs ev)
+        {
+            string temp = Plugin.Instance.Config.WaitTimeLCZ.ToString();
+            float time = float.Parse(temp);
+            Timing.CallDelayed(time, () =>
+            {
+                Cassie.Message(message: Plugin.Instance.Config.MessageLCZ, isSubtitles: Plugin.Instance.Config.IsSubtiteledLCZ);
+            });
+        }
+
+        public void OnRnStart()
+        {
+            if (Plugin.Instance.Config.WaitTimeRn == 0)
+            {
+                Timing.CallDelayed(1f, () =>
+                {
+                    Cassie.Message(message: Plugin.Instance.Config.MessageRound, isSubtitles: Plugin.Instance.Config.IsSubtiteledRound);
+                });
+            }
+            else
+            {
+                string temp = Plugin.Instance.Config.WaitTimeRn.ToString();
+                float time = float.Parse(temp);
+
+                Timing.CallDelayed(time, () =>
+                {
+                    Cassie.Message(message: Plugin.Instance.Config.MessageRound, isSubtitles: Plugin.Instance.Config.IsSubtiteledRound);
+                });
+            }
+        }
+
+        public void OnGenAct(GeneratorActivatedEventArgs ev)
+        {
+            int act = Generator.Get(GeneratorState.Engaged).Count();
+
+            if (act == 2)
+            {
+                string temp = Plugin.Instance.Config.WaitTimeGen.ToString();
+                float time = float.Parse(temp);
+                Timing.CallDelayed(time, () => Cassie.Message(Plugin.Instance.Config.MessageGen, isSubtitles: Plugin.Instance.Config.IsSubtiteledGen));
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using Exiled.API.Features;
+using System;
+
+namespace CustomCASSIE
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "Custom CASSIE";
+        public override string Prefix => "CustomCASSIE";
+        public override string Author => "VALERA771#1471";
+        public override Version Version => new Version(1, 1, 0);
+        public override Version RequiredExiledVersion => new Version(5, 3, 0);
+
+        private Handlers han;
+        public static int ActGen;
+
+        public override void OnEnabled()
+        {
+            han = new Handlers();
+            Exiled.Events.Handlers.Map.Decontaminating += han.OnDecont;
+            Exiled.Events.Handlers.Map.GeneratorActivated += han.OnGenAct;
+            Exiled.Events.Handlers.Server.RoundStarted += han.OnRnStart;
+            Plugin.singleton = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            han = null;
+            Exiled.Events.Handlers.Map.Decontaminating -= han.OnDecont;
+            Exiled.Events.Handlers.Map.GeneratorActivated -= han.OnGenAct;
+            Exiled.Events.Handlers.Server.RoundStarted -= han.OnRnStart;
+
+            Plugin.singleton = null;
+            base.OnDisabled();
+        }
+
+        public override void OnReloaded()
+        {
+            base.OnReloaded();
+        }
+
+
+        public static Plugin Instance => singleton;
+        private static Plugin singleton;
+    }
+}
